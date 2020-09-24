397cdf2ac65f4c465250489b3b948ca592ad798f
Patrick Fernie <patrick.fernie@gmail.com>
2020-09-24 14:23:31 -0400

    use `thiserror` in lib, and support `traced-error` feature

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,29 @@
 #![deny(warnings, missing_debug_implementations, rust_2018_idioms)]
 
 use color_eyre::eyre::{self, WrapErr};
+use thiserror::Error;
 use tracing::{event, info, instrument, span, warn, Level};
+#[cfg(feature = "traced-error")]
+use tracing_error::{prelude::*, TracedError};
+
+#[derive(Debug, Error)]
+pub struct Error {
+    #[error("unknown error")]
+    Unknown,
+}
+
+#[cfg(feature = "traced-error")]
+type Result<T, E = TracedError<Error>> = color_eyre::Result<T, E>;
+#[cfg(not(feature = "traced-error"))]
+type Result<T, E = Error> = color_eyre::Result<T, E>;
+
+macro_rules! err {
+    ($e:expr) => {
+        Err($e)
+        #[cfg(feature = "traced-error")]
+            .in_current_span()
+    };
+}
 
 /// The string literal `"hello, world!"`
 /// ```
